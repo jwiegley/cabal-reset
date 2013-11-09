46e0d529f5dd0d77320893e1bd758525eb737c7a
John Wiegley <johnw@newartisans.com>
2013-11-09 16:53:29 -0600

    Updated cabal-bootstrap.sh

--- cabal-bootstrap.sh
+++ cabal-bootstrap.sh
@@ -2,24 +2,21 @@
 
 export PATH=$HOME/bin:$HOME/.cabal/bin:$PATH
 
-rm -f /tmp/deps
-
-#cabal-reset.sh "$@"
-
 installed() {
     ghc-pkg latest $1 > /dev/null 2>&1
 }
 
 install() {
     echo cabal install "$@"
-    cabal install "$@"
+    cabal install --haddock-hoogle --haddock-html       \
+        --haddock-executables --haddock-internal        \
+        --haddock-hyperlink-source "$@"
 }
 
 install_prereqs() {
     if ! installed Cabal; then
         install Cabal cabal-install
     fi
-    test -x "$(which cabal-meta)" || install cabal-meta cabal-src
 
     if ! test -x "$(which alex)"; then
         install -j --disable-executable-dynamic alex
@@ -33,33 +30,37 @@ install_prereqs() {
     if ! test -x "$(which c2hs)"; then
         install -j c2hs
     fi
-    # if ! test -x "$(which ghc-heap-view)"; then
-    #     install -j ghc-heap-view --disable-library-profiling
-    # fi
 
     if ! installed text-icu; then
         DYLD_LIBRARY_PATH=/usr/local/opt/icu4c/lib                      \
-            install -j1 text-icu                                  \
+            install -j1 text-icu                                        \
                 --extra-include-dirs=/usr/local/opt/icu4c/include       \
                 --extra-lib-dirs=/usr/local/opt/icu4c/lib
     fi
-
     if ! installed libxml-sax; then
         PKG_CONFIG_PATH=/usr/local/opt/libxml2/lib/pkgconfig            \
-            install -j1 libxml-sax                                \
+            install -j1 libxml-sax                                      \
                 --extra-include-dirs=/usr/local/opt/libxml2/include     \
                 --extra-lib-dirs=/usr/local/opt/libxml2/lib
     fi
-
     if ! installed readline; then
-        DYLD_LIBRARY_PATH=/usr/local/opt/readline/lib                   \
-            install -j1 readline                                  \
-                --extra-include-dirs=/usr/local/opt/readline/include    \
-                --extra-lib-dirs=/usr/local/opt/readline/lib            \
-                --configure-option=--with-readline-includes=/usr/local/opt/readline/include \
+        DYLD_LIBRARY_PATH=/usr/local/opt/readline/lib                                           \
+            install -j1 readline                                                                \
+                --extra-include-dirs=/usr/local/opt/readline/include                            \
+                --extra-lib-dirs=/usr/local/opt/readline/lib                                    \
+                --configure-option=--with-readline-includes=/usr/local/opt/readline/include     \
                 --configure-option=--with-readline-libraries=/usr/local/opt/readline/lib
     fi
+    if ! installed libffi; then
+        DYLD_LIBRARY_PATH=/usr/local/opt/libffi/lib                     \
+        PKG_CONFIG_PATH=/usr/local/opt/libffi/lib/pkgconfig             \
+            install -j1 libffi                                          \
+                --extra-include-dirs=/usr/local/opt/libffi/include      \
+                --extra-lib-dirs=/usr/local/opt/libffi/lib
+    fi
+}
 
+install_postreqs() {
     if ! test -x "$(which gtk2hsTypeGen)"; then
         install -f have-quartz-gtk -j gtk2hs-buildtools
     fi
@@ -67,7 +68,7 @@ install_prereqs() {
         install -f have-quartz-gtk -j glib gtk cairo
     fi
     if ! test -x "$(which threadscope)"; then
-        install -f have-quartz-gtk -j threadscope splot timeplot
+        install -f have-quartz-gtk -j threadscope splot #timeplot
     fi
 }
 
@@ -78,77 +79,104 @@ do_cabal() {
         | perl -pe 's/-[0-9].+//;'
 }
 
-find ~/Contracts/ ~/Projects/ ~/Mirrors/ -maxdepth 1 -type d \
-    | while read dir ; do
-    if [[ -f $dir/sources.txt ]]; then
-        (cd $dir ; do_cabal cabal-meta >> /tmp/deps 2> /dev/null) || echo skip
-    elif [[ -f "$(echo $dir/*.cabal)" ]]; then
-        (cd $dir; do_cabal cabal >> /tmp/deps 2> /dev/null) || echo skip
-    fi
-done
+# if ! installed cabal-meta; then
+#     install cabal-meta
+# fi
+
+rm -f /tmp/deps
+
+# find ~/Contracts/ ~/Projects/ ~/Mirrors/ -maxdepth 1 -type d \
+#     | while read dir ; do
+#     if [[ -f $dir/sources.txt ]]; then
+#         (cd $dir ; do_cabal cabal-meta >> /tmp/deps 2> /dev/null) || echo skip
+#     elif [[ -f "$(echo $dir/*.cabal)" ]]; then
+#         (cd $dir; do_cabal cabal >> /tmp/deps 2> /dev/null) || echo skip
+#     fi
+# done
 
 cat >> /tmp/deps <<EOF
-HUnit
 doctest
 doctest-prop
 hspec
 hspec-expectations
-quickcheck
 
 simple-reflect
 pretty-show
 
+CC-delcont
 Boolean
+acid-state
 adjunctions
-async
+attempt
 bifunctors
 categories
 classy-prelude
 comonad
 comonad-transformers
+compdata
 composition
 cond
 conduit
 convertible
 distributive
+directory
 either
+esqueleto
+exceptions
+filepath
+filesystem-conduit
 free
-hashable
+http-conduit
+kan-extensions
 keys
 lens
 lifted-async
 lifted-base
+linear
 monad-control
 monad-coroutine
+monad-logger
 monad-loops
 monad-par
 monad-par-extras
 monad-stm
+monadloc
 monoid-extras
+newtype
 numbers
 operational
-parallel
+persistent
+persistent-sqlite
+persistent-postgresql
+persistent-template
 pointed
+posix-paths
+process
 profunctor-extras
 profunctors
 reducers
 reflection
+regex-applicative
+resource-pool
 resourcet
 retry
 rex
 safe
+safe-failure
+scotty
 semigroupoids
 semigroups
+simple-reflect
 snappy
 speculation
-split
 spoon
-stm
 stm-chans
 stm-conduit
 stm-stats
+strict
 system-fileio
 system-filepath
+tables
 tagged
 tagged-transformer
 these
@@ -159,69 +187,63 @@ configurator
 cpphs
 ekg
 hlint
-hscolour
 optparse-applicative
 orc
 shake
 shelly
 EOF
 
-if ! test -x "$(which cabal-meta)"; then
-    echo cabal-meta >> /tmp/deps
-    echo cabal-src >> /tmp/deps
-fi
+#    git-annex
+#    git-monitor
+#    hledger
+#    darcs
+#    idris
+#    agda
+#    lambdabot
+#    mueval
+#    unlambda
+#    pointfree
 
 for i in                                        \
-    agda                                        \
     c2hsc                                       \
-    cab                                         \
     cabal-db                                    \
     cabal-dev                                   \
-    darcs                                       \
     ghc-core                                    \
     git-all                                     \
-    git-annex                                   \
+    git-monitor                                 \
     hasktags                                    \
     hdevtools                                   \
-    hledger                                     \
     hlint                                       \
     hobbes                                      \
     hsenv                                       \
-    mueval                                      \
-    pandoc                                      \
-    pointfree                                   \
+    rehoo                                       \
     sizes                                       \
     stylish-haskell                             \
     una                                         \
-    unlambda                                    \
     yesod
 do
-    if ! test -x "$(which $i)"; then
+    if [[ ! -x "$(which $i)" ]]; then
         echo $i >> /tmp/deps
     fi
 done
 
 # Libraries that are currently broken
-for i in                                        \
-    cabal-file-th                               \
-    linear
+for i in cabal-file-th #linear
 do
-    perl -i -ne "print unless /$i/;" /tmp/deps
+    perl -i -ne "print unless /^$i/;" /tmp/deps
 done
 
-uniqify /tmp/deps
+# Libraries that are already installed
+for i in $(ghc-pkg list | egrep -v '(^/|\()' | sed 's/-[0-9].*//')
+do
+    perl -i -ne "print unless /^$i/;" /tmp/deps
+done
 
 install_prereqs
 
+uniqify /tmp/deps
 install "$@" -j $(< /tmp/deps) || (echo "Cabal build plain failed"; exit 1)
 
+#install_postreqs
+
 ghc-pkg check
-
-if [[ "$1" == --full ]]; then
-    rebuild-hoogle
-
-    cabal-reset.sh
-    cabal-bootstrap.sh
-fi
-
-exit 0
